4f15e5f69f7937390a7fbce564c975d6de86018b
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-04-07 03:52:08 +0600

    link

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Dashboard = () => {
   return (
     <div>
-        <h2>Dashboard</h2>
+        <Link to="/inward">go</Link>
     </div>
   )
 }
