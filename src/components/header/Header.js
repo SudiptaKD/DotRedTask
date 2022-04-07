845c0507ad4a40805b482b84533452572dc6c827
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-04-07 21:18:21 +0600

    Header updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import ApiArchive from '../icons/api.svg'
 import COURIER from '../icons/courier.svg'
 import ECOM from '../icons/ecom.svg'
-import FINANCE from '../icons/finance.svg'
 import LIST from '../icons/list.svg'
 import LOGOUT from '../icons/logout.svg'
 import WMS from '../icons/wms.svg'
@@ -14,7 +14,7 @@ export default function Header() {
       <Container style={{'min-heigt':'300px',
          'padding':'20px 50px', 'max-width':'100%' }}>
         <Row>
-          <Col className='text-success d-flex justify-content-start' lg={3}>
+          <Col className='text-success d-flex justify-content-start' lg={2}>
             <Button size="sm" variant='secondary' style={{'marginRight':'4px'}}>
               <img src={LIST} alt="list" style={{'width':'20px', 'height':'20px'}} />
             </Button>{' '}
@@ -23,25 +23,27 @@ export default function Header() {
               {' '} API Status
             </Button>
           </Col>
-          <Col className=' d-flex justify-content-center' lg={6}>
-            <Button size="sm" variant='secondary' style={{'marginRight':'2px'}}>
-              <img src={ECOM} alt="ecom" style={{'width':'20px', 'height':'20px'}} />
-              {' '} ECOM
-            </Button>
-            <Button size="sm" variant='secondary' style={{'marginRight':'2px'}}>
-              <img src={COURIER} alt="courier" style={{'width':'20px', 'height':'20px'}} />
-              {' '} COURIER
-            </Button>
-            <Button size="sm" variant='secondary' style={{'marginRight':'2px'}}>
-              <img src={WMS} alt="wms" style={{'width':'20px', 'height':'20px'}} />
-              {' '} WMS
-            </Button>
-            <Button size="sm" variant='secondary' style={{'marginRight':'2px'}}>
-              <img src={FINANCE} alt="finance" style={{'width':'20px', 'height':'20px'}} />
-              {' '} FINANCE
-            </Button>
+          <Col className=' d-flex justify-content-center' lg={8}>
+            <Link to="/dashboard" style={{'textDecoration':'none'}}>
+              <Button size="sm" variant='secondary' style={{'marginRight':'5px', 'minWidth':'9rem'}}>
+                <img src={ECOM} alt="ecom" style={{'width':'20px', 'height':'20px'}} />
+                {' '} Dashboard Page
+              </Button>
+            </Link>
+            <Link to="/inward" style={{'textDecoration':'none'}}>
+              <Button size="sm" variant='info' style={{'marginRight':'5px', 'minWidth':'9rem'}}>
+                <img src={COURIER} alt="courier" style={{'width':'20px', 'height':'20px'}} />
+                {' '} Inward Page
+              </Button>
+            </Link>
+            <Link to="/order" style={{'textDecoration':'none'}}>
+              <Button size="sm" variant='success' style={{'marginRight':'5px', 'minWidth':'9rem'}}>
+                <img src={WMS} alt="wms" style={{'width':'20px', 'height':'20px'}} />
+                {' '} Order Page
+              </Button>
+            </Link>
           </Col>
-          <Col className=' d-flex justify-content-end' lg={3}>
+          <Col className=' d-flex justify-content-end' lg={2}>
             <Button size="sm" variant='secondary'>
               <img src={LOGOUT} alt="logout" style={{'width':'20px', 'height':'20px'}} />
               {' '} Logout
